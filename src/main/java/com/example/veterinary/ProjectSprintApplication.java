87538f1f54e28b7750b8497308b6e77dda49fe76
Danielcasaspira <hdaniel0519@hotmail.com>
2022-06-02 16:15:48 -0500

    se agrega swagger a proyecto spring

---
+++ src/main/java/com/example/veterinary/ProjectSprintApplication.java
@@ -0,0 +1,12 @@
+package com.example.veterinary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSprintApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/veterinary/entity/Animal.java
@@ -0,0 +1,49 @@
+package com.example.veterinary.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="animal")
+public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_animal")
+    private Long idAnimal;
+    private String name;
+    private Integer old;
+    private String species;
+
+    public Long getIdAnimal() {
+        return idAnimal;
+    }
+
+    public void setIdAnimal(Long idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getOld() {
+        return old;
+    }
+
+    public void setOld(Integer old) {
+        this.old = old;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+}
